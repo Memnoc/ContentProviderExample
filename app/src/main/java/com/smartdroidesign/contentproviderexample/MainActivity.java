750dcaa850ba289545f533897b286fbe911caa1f
Memnoc <smartdroidesign@gmail.com>
2019-03-19 15:17:25 +0000

    JAVA - ContentResolver and Cursor

--- app/src/main/java/com/smartdroidesign/contentproviderexample/MainActivity.java
+++ app/src/main/java/com/smartdroidesign/contentproviderexample/MainActivity.java
@@ -1,15 +1,22 @@
 package com.smartdroidesign.contentproviderexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ListView contactNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +25,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        contactNames = findViewById(R.id.contact_names);
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.d(TAG, "fab onClick: starts");
+                String[] projection = {ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
+                // Content Resolver is queried for data, and it returns a cursor.
+                ContentResolver contentResolver = getContentResolver();
+                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, // Source of data.
+                        projection, // String[] array to store the name of the columns retrieved.
+                        null, // String that contains a filter to determine which rows are returned. (It's a WHERE clause, and null = get me all rows(
+                        null, // Array of values used to replace the placeholders in the selection string
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY); // This is like the ORDER BY clause
             }
         });
     }
